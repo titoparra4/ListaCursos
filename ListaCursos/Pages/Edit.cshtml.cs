0aa929dfec45ce56a552e064c61810c05d2642be
Tito <tito.parra4@@hotmail.com>
2020-02-22 22:05:05 +0100

    Create Page

--- ListaCursos/Pages/Edit.cshtml.cs
+++ ListaCursos/Pages/Edit.cshtml.cs
@@ -21,13 +21,21 @@ namespace ListaCursos.Pages
             this.coursesProvider = coursesProvider;
         }
 
-        public async Task<IActionResult> OnGet(int id)
+        public async Task<IActionResult> OnGet(int? id)
         {
-            var course = await coursesProvider.GetAsync(id);
-            if(course != null)
+            if(id == null)
             {
-                Course = course;
+                Course = new Course();
             }
+            else
+            {
+                var course = await coursesProvider.GetAsync(id.Value);
+                if (course != null)
+                {
+                    Course = course;
+                }
+            }
+
 
             return Page();
         }
@@ -39,12 +47,28 @@ namespace ListaCursos.Pages
                 return Page();
             }
 
-            var result = await coursesProvider.UpdateAsync(Course.Id, Course);
-
-            if (result)
+            if(Course.Id == 0)
             {
-                return RedirectToPage("Courses");
+                var result = await coursesProvider.AddAsync(Course);
+
+                if(result.IsSuccess)
+                {
+                    return RedirectToPage("Courses");
+                }
+
+                return Page();
             }
+            else
+            {
+
+                var result = await coursesProvider.UpdateAsync(Course.Id, Course);
+
+                if (result)
+                {
+                    return RedirectToPage("Courses");
+                }
+            }
+
 
             return Page();
         }
--- ListaCursos/Provider/FakeCoursesProvider.cs
+++ ListaCursos/Provider/FakeCoursesProvider.cs
@@ -37,6 +37,15 @@ namespace ListaCursos.Provider
                 Uri = "https://titoparra.com"
             });
         }
+
+        public Task<(bool IsSuccess, int? Id)> AddAsync(Course course)
+        {
+
+            course.Id = repo.Max(c => c.Id) + 1;
+            repo.Add(course);
+            return Task.FromResult((true, (int?)course.Id));
+        }
+
         public Task<ICollection<Course>> GetAllAsync()
         {
             return Task.FromResult((ICollection<Course>)repo.ToList());
