b294404fbc36cfccfa6cf06c2089110c0c3672ce
Tito <tito.parra4@@hotmail.com>
2020-02-22 21:41:03 +0100

    Edit page

--- ListaCursos/Interfaces/ICoursesProvider.cs
+++ ListaCursos/Interfaces/ICoursesProvider.cs
@@ -13,5 +13,7 @@ namespace ListaCursos.Interfaces
         Task<ICollection<Course>> SearchAsync(string search);
 
         Task<Course> GetAsync(int id);
+
+        Task<bool> UpdateAsync(int id, Course course);
     }
 }
---
+++ ListaCursos/Pages/Edit.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListaCursos.Interfaces;
+using ListaCursos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ListaCursos.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ICoursesProvider coursesProvider;
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        public EditModel(ICoursesProvider coursesProvider)
+        {
+            this.coursesProvider = coursesProvider;
+        }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            var course = await coursesProvider.GetAsync(id);
+            if(course != null)
+            {
+                Course = course;
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await coursesProvider.UpdateAsync(Course.Id, Course);
+            if (result)
+            {
+                return RedirectToPage("Courses");
+            }
+
+            return Page();
+        }
+    }
+}
